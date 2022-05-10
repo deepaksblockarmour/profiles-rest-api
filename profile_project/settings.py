8dab223ce1f6b920016b99a0640123b15d3cc8fe
Deepak <deepaks@blockarmour.com>
2022-05-10 16:22:41 +0530

    Added custom user profile model model,manager and migrations

--- profile_project/settings.py
+++ profile_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework",
+    "rest_framework.authtoken",
+    "profiles_api",
 ]
 
 MIDDLEWARE = [
@@ -121,5 +124,7 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+AUTH_USER_MODEL="profiles_api.UserProfile"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
