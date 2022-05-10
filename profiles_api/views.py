8787d86bf1658c742a80063452582a3658e36789
Deepak <deepaks@blockarmour.com>
2022-05-10 17:58:01 +0530

    errors

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,11 +1,17 @@
+from unicodedata import name
 from urllib import response
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profile_api import seriallizers
 
 class HelloApiView(APIView):
     '''test api view'''
-    
-    def get(sellf,request,format=None):
+    serializer_class=serializer.HelloSerializer
+
+
+    def get(self,request,format=None):
         """returns a list of api view"""
         an_apiview=[
             "uses http methods as function(get,post,patch,put,delete) ",
@@ -14,3 +20,16 @@ class HelloApiView(APIView):
             "is mapped manually to urls"
         ]
         return Response({'key':'hello','an_apiview':an_apiview})
+
+    def post(self,request):
+        """create  a hello message with our name"""
+        serializer=self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name=serializer.validated_data.get("name")
+            message=f'hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+            )
